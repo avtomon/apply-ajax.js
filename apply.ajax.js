7ae34873099eec5cc283d333c83d462eb83d8e9b
avtomon <avtomon86@gmail.com>
2016-08-10 16:33:34 +0300

    update 10.08.2016
    Update code and add comments

--- apply.ajax.js
+++ apply.ajax.js
@@ -2,41 +2,52 @@
  * Created by Александр on 05.05.14.
  */
 
+    const HIDE_CLASS = 'clone';
+
     /**
-     * Проверяет является ли строка JSON-объектом
+     * Проверка и парсинг JSON-строки
      *
-     * @param str - проверяемая строка
-     * @returns {*}
+     * @param str - json-строка
+     *
+     * @returns {boolean}
      */
-    function isJSON(str) {
-        try {
+    function isJSON(str)
+    {
+        try
+        {
             var result = JSON.parse(str);
-        } catch (e) {
+        }
+        catch (e)
+        {
             return false;
         }
         return result;
     }
 
     /**
-     * AJAX-запрос к серверу
+     * Умная обертка к Ajax-запросу к серверу
      *
      * @param params - параметры запроса к серверу
-     * @param async - асинхронный ли запрос отправлять
-     * @param type - тип запроса, GET, POST и т. п.
-     * @param callback - обработчик успешного завершения запроса
-     * @param callbackError - обработчик ошибочного завершения запроса
+     * @param async - асинхронно ли отправляться запрос?
+     * @param type - тип запроса (обычно GET или POST)
+     *
+     * @param callback - функция, отрабатывающая при успешном запросе
+     * @param callbackError - функция, отрабатывающая при ошибочном результате запроса
      */
     function request (params, async, type, callback, callbackError)
     {
-        if (callbackError)
-        {
-            error = callbackError;
-        }
+        params.get_instance = params.get_instance || false;
+        params.static_method = params.static_method || false;
+        params.pagecache_flush = params.pagecache_flush || false;
+
+        var error = callbackError ? callbackError : window.error || null;
+
         $.ajax({
             type: type,
             url: "/router.php",
             dataType: 'json',
-            data: {
+            data:
+            {
                 JSONData: JSON.stringify(params)
             },
             async: async,
@@ -46,27 +57,20 @@
                 {
                     error('Произошла ошибка: ' + data.error);
                 }
-                else if (data.redirect !== undefined)
+                else if (data.redirect)
                 {
                     window.location = data.redirect;
                 }
                 else if (data.success !== undefined)
                 {
-                    if (callback !== undefined)
-                    {
-                        callback(data);
-                    }
-                    else
-                    {
-                        //Сообщение об успешном запросе при отсутствии обработчика
-                    }
+                    callback ? callback(data) : (window.showOk ? showOk('Запрос успешно выполнен') : null);
                 }
                 else
                 {
                     error('Произошла ошибка');
                 }
             },
-            error: function (XMLHttpRequest, textStatus, errorThrown)
+            error: function (XMLHttpRequest, textStatus)
             {
                 error('Произошла ошибка ' + textStatus);
             }
@@ -74,41 +78,42 @@
     }
 
     /**
-     * Вставить в шаблон несколько однородных записей (при этом на каждую запись создается копия DOM-объекта-родителя)
+     * Запилить данные в шаблон. При этом исходные элементы для вставки остаются неизменными и становятся невидимыми, в том время как данные
+     * вставляются в их видимые копии, которые создаются перед вставкой
      *
      * @param data - данные для вставки
-     * @param parent - DOM-объект в который нужно вставить данные
+     * @param parent - jQuery-селектор блоков для вставки
      *
-     * @returns {XML|*}
+     * @returns {*}
      */
     function setMultiData(data, parent)
     {
-        if (data)
+        if (data && data.success !== undefined)
         {
-            if (data.success !== undefined)
-                data = data.success;
+            data = data.success;
+            if (data[0] === undefined)
+                data = [data];
 
-            if (data && !data.error && typeof(data) === 'object')
+            if (typeof(data) === 'object')
             {
-                var set_length = data.length;
-                for (var i = 0; i < set_length; i++)
+                for(var j in parent)
                 {
-                    var element = parent.eq(0).clone(true).appendTo(parent.parent());
-                    if (data[i])
+                    var p = parent.eq(j).addClass(HIDE_CLASS);
+                    for (var i in data)
                     {
-                        setData(data[i], element);
+                        setData(data[i], p.clone(true).appendTo(p.parent()));
                     }
                 }
             }
         }
-        return parent.parent();
+        return parent.eq(0).parent();
     }
 
     /**
-     * Вставить данные в DOM-объект шаблона
+     * Запилить данные в шаблон. В отличие от предыдущей функции, здесь данные вставляются прямо в выбранные селектором элементы
      *
      * @param data - данные для вставки
-     * @param parent - DOM-объект
+     * @param parent - jQuery-селектор блоков для вставки
      *
      * @returns {*}
      */
@@ -124,140 +129,134 @@
                 if (data[0] !== undefined)
                     data = data[0];
 
-                parent.removeClass('clone');
+                parent.removeClass(HIDE_CLASS);
 
-                var objkeys = Object.keys(data);
-                var len = objkeys.length;
+                var objkeys = Object.keys(data),
+                    len = objkeys.length;
+
                 for (var i = 0; i < len; i++)
                 {
-                    var objkey = objkeys[i];
-                    var tmp = isJSON(data[objkey]);
+                    var objkey = objkeys[i],
+                        tmp = isJSON(data[objkey]);
+
                     if (tmp && tmp.length)
                     {
                         data[objkey] = tmp;
                     }
                     if (typeof(data[objkey]) === 'object' && data[objkey] !== null)
                     {
-                        setMultiData(data[objkey], parent.find('.' + objkey));
+                        setMultiData(data[objkey], parent.find('.' + objkey + '.parent'));
                     }
-                    if (parent.hasClass('in_id_' + objkey))
+                    else
                     {
-                        parent.attr('id', data[objkey]);
-                    }
-                    if (parent.hasClass('in_name_' + objkey))
-                    {
-                        parent.attr('name', data[objkey]);
-                        //parent.attr('name', data[objkey]);
-                    }
-                    if (parent.hasClass('in_class_' + objkey))
-                    {
-                        parent.addClass(data[objkey].toString());
-                    }
-                    if (parent.hasClass('in_val_' + objkey) || parent.hasClass('in_value_' + objkey))
-                    {
-                        parent.val(data[objkey]);
-                    }
-                    if (parent.hasClass('in_title_' + objkey))
-                    {
-                        parent.attr('title', data[objkey]);
-                    }
-                    if (parent.hasClass('in_text_' + objkey))
-                    {
-                        parent.text(data[objkey]);
-                    }
-                    if (parent.hasClass('in_html_' + objkey))
-                    {
-                        parent.html(data[objkey]);
-                    }
-                    if (parent.hasClass('in_href_' + objkey))
-                    {
-                        parent.attr('href', parent.attr('href') ? parent.attr('href') + data[objkey] : data[objkey]);
-                    }
-                    if (parent.hasClass('in_src_' + objkey) && data[objkey])
-                    {
-                        parent.attr('src', data[objkey]);
-                    }
-                    if (parent.hasClass('in_alt_' + objkey))
-                    {
-                        parent.attr('alt', data[objkey]);
-                    }
-                    if (parent.hasClass('in_data-val_' + objkey))
-                    {
-                        parent.attr('data-val', data[objkey]);
-                    }
-                    if (parent.hasClass('in_for_' + objkey))
-                    {
-                        parent.attr('for', data[objkey]);
-                    }
-                    if (parent.hasClass('in_type_' + objkey))
-                    {
-                        parent.attr('type', data[objkey]);
-                    }
-                    if (parent.hasClass('in_prop_' + objkey))
-                    {
-                        parent.prop(data[objkey], true);
-                    }
+                        if (parent.hasClass('in_id_' + objkey))
+                        {
+                            parent.attr('id', data[objkey]);
+                        }
+                        if (parent.hasClass('in_name_' + objkey))
+                        {
+                            parent.attr('name', data[objkey]);
+                        }
+                        if (parent.hasClass('in_class_' + objkey))
+                        {
+                            parent.addClass(data[objkey].toString());
+                        }
+                        if (parent.hasClass('in_val_' + objkey) || parent.hasClass('in_value_' + objkey))
+                        {
+                            parent.val(data[objkey]);
+                        }
+                        if (parent.hasClass('in_title_' + objkey))
+                        {
+                            parent.attr('title', data[objkey]);
+                        }
+                        if (parent.hasClass('in_text_' + objkey))
+                        {
+                            parent.html(data[objkey]);
+                        }
+                        if (parent.hasClass('in_href_' + objkey))
+                        {
+                            parent.attr('href', parent.attr('href') ? parent.attr('href') + data[objkey] : data[objkey]);
+                        }
+                        if (parent.hasClass('in_src_' + objkey) && data[objkey])
+                        {
+                            parent.attr('src', data[objkey]);
+                        }
+                        if (parent.hasClass('in_alt_' + objkey))
+                        {
+                            parent.attr('alt', data[objkey]);
+                        }
+                        if (parent.hasClass('in_data-val_' + objkey))
+                        {
+                            parent.attr('data-val', data[objkey]);
+                        }
+                        if (parent.hasClass('in_for_' + objkey))
+                        {
+                            parent.attr('for', data[objkey]);
+                        }
+                        if (parent.hasClass('in_type_' + objkey))
+                        {
+                            parent.attr('type', data[objkey]);
+                        }
+                        if (parent.hasClass('in_prop_' + objkey))
+                        {
+                            parent.prop(data[objkey], true);
+                        }
 
 
-                    if (parent.find('.in_id_' + objkey).length)
-                    {
-                        parent.find('.in_id_' + objkey).attr('id', data[objkey]);
-                        //parent.find('.in_id_' + objkey).attr('name', data[objkey]);
-                    }
-                    if (parent.find('.in_name_' + objkey).length)
-                    {
-                        parent.find('.in_name_' + objkey).attr('name', data[objkey]);
-                        //parent.find('.in_id_' + objkey).attr('name', data[objkey]);
-                    }
-                    if (parent.find('.in_class_' + objkey).length)
-                    {
-                        parent.find('.in_class_' + objkey).addClass(data[objkey].toString());
-                    }
-                    if (parent.find('.in_val_' + objkey).length || parent.find('.in_value_' + objkey).length)
-                    {
-                        parent.find('.in_val_' + objkey + ', .in_value_' + objkey).val(data[objkey]);
-                    }
-                    if (parent.find('.in_title_' + objkey).length)
-                    {
-                        parent.find('.in_title_' + objkey).attr('title', data[objkey]);
-                    }
-                    if (parent.find('.in_text_' + objkey).length)
-                    {
-                        parent.find('.in_text_' + objkey).html(data[objkey]);
-                    }
-                    if (parent.find('.in_html_' + objkey).length)
-                    {
-                        parent.find('.in_html_' + objkey).html(data[objkey]);
-                    }
-                    if (parent.find('.in_href_' + objkey).length)
-                    {
-                        if (parent.find('.in_href_' + objkey).attr('href') === undefined)
-                            parent.find('.in_href_' + objkey).attr('href', '');
-                        parent.find('.in_href_' + objkey).attr('href', parent.find('.in_href_' + objkey).attr('href') + data[objkey]);
-                    }
-                    if (parent.find('.in_src_' + objkey).length && data[objkey])
-                    {
-                        parent.find('.in_src_' + objkey).attr('src', data[objkey]);
-                    }
-                    if (parent.find('.in_alt_' + objkey).length)
-                    {
-                        parent.find('.in_alt_' + objkey).attr('alt', data[objkey]);
-                    }
-                    if (parent.find('.in_data-val_' + objkey).length)
-                    {
-                        parent.find('.in_data-val_' + objkey).attr('data-val', data[objkey]);
-                    }
-                    if (parent.find('.in_for_' + objkey).length)
-                    {
-                        parent.find('.in_for_' + objkey).attr('for', data[objkey]);
-                    }
-                    if (parent.find('.in_type_' + objkey).length)
-                    {
-                        parent.find('.in_type_' + objkey).attr('type', data[objkey]);
-                    }
-                    if (parent.find('.in_prop_' + objkey).length)
-                    {
-                        parent.find('.in_prop_' + objkey).prop(data[objkey], true);
+                        if (parent.find('.in_id_' + objkey).length)
+                        {
+                            parent.find('.in_id_' + objkey).attr('id', data[objkey]);
+                        }
+                        if (parent.find('.in_name_' + objkey).length)
+                        {
+                            parent.find('.in_name_' + objkey).attr('name', data[objkey]);
+                        }
+                        if (parent.find('.in_class_' + objkey).length)
+                        {
+                            parent.find('.in_class_' + objkey).addClass(data[objkey].toString());
+                        }
+                        if (parent.find('.in_val_' + objkey).length || parent.find('.in_value_' + objkey).length)
+                        {
+                            parent.find('.in_val_' + objkey + ', .in_value_' + objkey).val(data[objkey]);
+                        }
+                        if (parent.find('.in_title_' + objkey).length)
+                        {
+                            parent.find('.in_title_' + objkey).attr('title', data[objkey]);
+                        }
+                        if (parent.find('.in_text_' + objkey).length)
+                        {
+                            parent.find('.in_text_' + objkey).html(data[objkey]);
+                        }
+                        if (parent.find('.in_href_' + objkey).length)
+                        {
+                            if (parent.find('.in_href_' + objkey).attr('href') === undefined)
+                                parent.find('.in_href_' + objkey).attr('href', '');
+                            parent.find('.in_href_' + objkey).attr('href', parent.find('.in_href_' + objkey).attr('href') + data[objkey]);
+                        }
+                        if (parent.find('.in_src_' + objkey).length && data[objkey])
+                        {
+                            parent.find('.in_src_' + objkey).attr('src', data[objkey]);
+                        }
+                        if (parent.find('.in_alt_' + objkey).length)
+                        {
+                            parent.find('.in_alt_' + objkey).attr('alt', data[objkey]);
+                        }
+                        if (parent.find('.in_data-val_' + objkey).length)
+                        {
+                            parent.find('.in_data-val_' + objkey).attr('data-val', data[objkey]);
+                        }
+                        if (parent.find('.in_for_' + objkey).length)
+                        {
+                            parent.find('.in_for_' + objkey).attr('for', data[objkey]);
+                        }
+                        if (parent.find('.in_type_' + objkey).length)
+                        {
+                            parent.find('.in_type_' + objkey).attr('type', data[objkey]);
+                        }
+                        if (parent.find('.in_prop_' + objkey).length)
+                        {
+                            parent.find('.in_prop_' + objkey).prop(data[objkey], true);
+                        }
                     }
                 }
             }
@@ -268,4 +267,30 @@
         {
             return false;
         }
+    }
+
+    /**
+     * Использовать один из двух предудущих методов вставки изходя из присутствия в старших тегах блоков для вставки класса, отвечающего
+     * за сокрытие эталонных блоков (HIDE_CLASS)
+     *
+     * @param data - данные для вставки
+     * @param parent - jQuery-селектор блоков для вставки
+     *
+     * @returns {boolean}
+     */
+    function insertData(data, parent)
+    {
+        for (var j in parent)
+        {
+            var p = parent.eq(j);
+            if (p.hasClass(HIDE_CLASS))
+            {
+                setMultiData(data, p);
+            }
+            else
+            {
+                setData(data, p);
+            }
+        }
+        return true;
     }
